202b522c1c63bcfe8842b2eb4e03c054fca55faf
Philip <membaphilip1@gmail.com>
2024-10-11 23:22:37 +0300

    done with the post

--- server/app.py
+++ server/app.py
@@ -52,6 +52,52 @@ def get_pizzas():
     pizzas_data=[pizza.to_dict(only=("id",'name','ingredients'))for pizza in pizzas]
     return make_response(pizzas_data),200
 
+@app.route("/restaurant_pizzas",methods=['POST'])
+def create_restaurant_pizza():
+    data=request.get_json()
+
+    price=data.get('price')
+    pizza_id=data.get('pizza_id')
+    restaurant_id=data.get('restaurant_id')
+
+    errors=[]
+    if price is None or not isinstance(price,(int,float)) or not(1<=price<=30):
+        errors.append("price must be a number")
+    if pizza_id is None:
+        errors.append("pizza id is required")
+    if restaurant_id is None:
+        errors.append("restaurant id is required")
+
+    
+    if errors:
+        return make_response({ "errors": ["validation errors"]},400)
+    
+    try:
+        new_restaurant_pizza=RestaurantPizza(price=price,pizza_id=pizza_id,restaurant_id=restaurant_id)
+        db.session.add(new_restaurant_pizza)
+        db.session.commit()
+        restaurant_pizza_data = {
+        "id": new_restaurant_pizza.id,
+        "price": new_restaurant_pizza.price,
+        "pizza_id": new_restaurant_pizza.pizza_id,
+        "restaurant_id": new_restaurant_pizza.restaurant_id,
+        "pizza": {
+            "id": new_restaurant_pizza.pizza.id,
+            "name": new_restaurant_pizza.pizza.name,
+            "ingredients": new_restaurant_pizza.pizza.ingredients
+        },
+        "restaurant": {
+            "id": new_restaurant_pizza.restaurant.id,
+            "name": new_restaurant_pizza.restaurant.name,
+            "address": new_restaurant_pizza.restaurant.address
+        }
+        
+    }
+        return make_response(restaurant_pizza_data),201
+    except Exception as e:
+        return make_response({"errors":[str(e)]}),400
+        
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
